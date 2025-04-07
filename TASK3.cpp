a1f424483e07211be18ef0fae2a2560bd23b052f
Noor e Sahar <saharenoor518@gmail.com>
2025-04-07 21:38:01 +0500

    Update TASK3.cpp

--- TASK3.cpp
+++ TASK3.cpp
@@ -1,3 +1,34 @@
+/*
+In computer science, linear search or sequential search is a method for finding a particular value in a list
+that consists of checking every one of its elements, one at a time and in sequence, until the desired one is
+found. The pseudocode of the task is given below:
+For I = 0:N-1
+If ( A[I] == Value )
+Return [I];
+End-if
+End-For
+return -1;
+Implement the code for linear sort using templates. Test your code using the main function below and
+upload it to your repository: Lab-1-DSA. The name of the code on your repository should be Task_3.cpp
+int main() {
+// Test with an integer array of size 5
+int intArray[5] = {64, 25, 12, 22, 11};
+int intKey = 12;
+int intIndex = linearSearch(intArray, intKey);
+printSearchResult(intIndex, intKey);
+// Test with a float array of size 4
+float floatArray[4] = {3.14, 2.71, 1.62, 0.57};
+float floatKey = 1.62;
+int floatIndex = linearSearch(floatArray, floatKey);
+printSearchResult(floatIndex, floatKey);
+// Test with a string array of size 4
+string stringArray[4] = {"apple", "orange", "banana", "grape"};
+string stringKey = "banana";
+int stringIndex = linearSearch(stringArray, stringKey);
+printSearchResult(stringIndex, stringKey);
+return 0;
+}
+*/
 #include <iostream>
 #include <string>
 using namespace std;
